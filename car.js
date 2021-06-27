6e55a0a654b07b21f1a97d5171bf96b6c85c4832
YEK-PLUS <yunusemre@sariilanlar.com>
2021-06-27 22:09:16 +0300

    day 1

--- car.js
+++ car.js
@@ -10,20 +10,20 @@ const reverseFactor = 0.0005;
 const drag = 0.95;
 const angularDrag = 0.95;
 const turnSpeed = 0.002;
-
+let gg = false;
 // Key codes
 
 const arrowKeys = {
   up: 38,
   down: 40,
   left: 37,
-  right: 39
+  right: 39,
 };
 const wasdKeys = {
   up: 87,
   down: 83,
   left: 65,
-  right: 68
+  right: 68,
 };
 
 const keyActive = (key) => {
@@ -33,13 +33,13 @@ const keyActive = (key) => {
 let windowWidth = window.innerWidth;
 let windowHeight = window.innerHeight;
 
-const canvas = document.getElementsByTagName('canvas')[0];
-const ctx = canvas.getContext('2d');
+const canvas = document.getElementsByTagName("canvas")[0];
+const ctx = canvas.getContext("2d");
 
-const scene = document.getElementsByClassName('scene')[0];
+const scene = document.getElementsByClassName("scene")[0];
 
 const localCar = {
-  el: document.getElementsByClassName('car')[0],
+  el: document.getElementsByClassName("car")[0],
   x: windowWidth / 2,
   y: windowHeight / 2,
   xVelocity: 0,
@@ -49,88 +49,50 @@ const localCar = {
   angle: 0,
   angularVelocity: 0,
   isThrottling: false,
-  isReversing: false
+  isReversing: false,
 };
 
 const cars = [localCar];
 const carsById = {};
 
-const keysDown = {};
+const keysDown = {
+  38: true,
+  87: true,
+};
 
 let needResize;
 let resizing;
 
-window.addEventListener('keydown', e => {
+//wasd arrow keys handler
+window.addEventListener("keydown", (e) => {
   keysDown[e.which] = true;
 });
 
-window.addEventListener('keyup', e => {
+window.addEventListener("keyup", (e) => {
   keysDown[e.which] = false;
 });
+//wasd arrow keys handler
 
 const touching = {
   up: 0,
   down: 0,
   left: 0,
-  right: 0
+  right: 0,
+};
+const gameover = () => {
+  //oyun bitti
+  localCar.x = windowWidth / 2;
+  localCar.y = windowHeight / 2;
+  localCar.xVelocity = 0;
+  localCar.yVelocity = 0;
+  localCar.power = 0;
+  localCar.reverse = 0;
+  localCar.angle = 0;
+  localCar.angularVelocity = 0;
+  gg = true;
 };
 
-window.addEventListener('touchstart', e => {
-  e.preventDefault();
-
-  if (touching.active) {
-    return;
-  }
-  touching.active = true;
-
-  const prevPos = {
-    x: e.touches[0].pageX,
-    y: e.touches[0].pageY
-  };
-
-  const touchmove = e => {
-    e.preventDefault();
-
-    const pos = {
-      x: e.touches[0].pageX,
-      y: e.touches[0].pageY
-    };
-
-    const diff = {
-      x: pos.x - prevPos.x,
-      y: pos.y - prevPos.y
-    };
-
-    prevPos.x = pos.x;
-    prevPos.y = pos.y;
-
-    touching.up -= diff.y / (windowHeight / 3);
-    touching.down += diff.y / (windowHeight / 3);
-    touching.left -= diff.x / (windowWidth / 3);
-    touching.right += diff.x / (windowWidth / 3);
-
-    touching.up = Math.max(0, Math.min(1, touching.up));
-    touching.down = Math.max(0, Math.min(1, touching.down));
-    touching.left = Math.max(0, Math.min(1, touching.left));
-    touching.right = Math.max(0, Math.min(1, touching.right));
-  };
-
-  const touchend = e => {
-    touching.active = false;
-    touching.up = 0;
-    touching.down = 0;
-    touching.left = 0;
-    touching.right = 0;
-
-    window.removeEventListener('touchmove', touchmove);
-    window.removeEventListener('touchend', touchend);
-  };
-
-  window.addEventListener('touchmove', touchmove);
-  window.addEventListener('touchend', touchend);
-});
-
-function updateCar (car, i) {
+function updateCar(car, i) {
   if (car.isThrottling) {
     car.power += powerFactor * car.isThrottling;
   } else {
@@ -165,7 +127,7 @@ function updateCar (car, i) {
   car.angularVelocity *= angularDrag;
 }
 
-function update () {
+function update() {
   cars.forEach(updateCar);
 }
 
@@ -174,16 +136,17 @@ let acc = 0;
 const step = 1 / 120;
 
 setInterval(() => {
-  let changed;
-
+  if (gg) return false;
   const canTurn = localCar.power > 0.0025 || localCar.reverse;
 
   if (touching.active) {
     const throttle = Math.round(touching.up * 10) / 10;
     const reverse = Math.round(touching.down * 10) / 10;
 
-    if (localCar.isThrottling !== throttle || localCar.isReversing !== reverse) {
-      changed = true;
+    if (
+      localCar.isThrottling !== throttle ||
+      localCar.isReversing !== reverse
+    ) {
       localCar.isThrottling = throttle;
       localCar.isReversing = reverse;
     }
@@ -191,50 +154,44 @@ setInterval(() => {
     const turnRight = canTurn && Math.round(touching.right * 10) / 10;
 
     if (localCar.isTurningLeft !== turnLeft) {
-      changed = true;
       localCar.isTurningLeft = turnLeft;
     }
     if (localCar.isTurningRight !== turnRight) {
-      changed = true;
       localCar.isTurningRight = turnRight;
     }
   } else {
-    const pressingUp = keyActive('up');
-    const pressingDown = keyActive('down');
+    const pressingUp = keyActive("up");
+    const pressingDown = keyActive("down");
 
-    if (localCar.isThrottling !== pressingUp || localCar.isReversing !== pressingDown) {
-      changed = true;
+    if (
+      localCar.isThrottling !== pressingUp ||
+      localCar.isReversing !== pressingDown
+    ) {
       localCar.isThrottling = pressingUp;
       localCar.isReversing = pressingDown;
     }
 
-    const turnLeft = canTurn && keyActive('left');
-    const turnRight = canTurn && keyActive('right');
+    const turnLeft = canTurn && keyActive("left");
+    const turnRight = canTurn && keyActive("right");
 
     if (localCar.isTurningLeft !== turnLeft) {
-      changed = true;
       localCar.isTurningLeft = turnLeft;
     }
     if (localCar.isTurningRight !== turnRight) {
-      changed = true;
       localCar.isTurningRight = turnRight;
     }
   }
 
   if (localCar.x > windowWidth) {
-    localCar.x -= windowWidth;
-    changed = true;
+    gameover();
   } else if (localCar.x < 0) {
-    localCar.x += windowWidth;
-    changed = true;
+    gameover();
   }
 
   if (localCar.y > windowHeight) {
-    localCar.y -= windowHeight;
-    changed = true;
+    gameover();
   } else if (localCar.y < 0) {
-    localCar.y += windowHeight;
-    changed = true;
+    gameover();
   }
 
   const ms = Date.now();
@@ -249,43 +206,52 @@ setInterval(() => {
   }
 
   lastTime = ms;
-
-  if (changed) {
-    sendParams(localCar);
-  }
 }, 1000 / 60);
 
-function randomizeCarColour (el) {
+function randomizeCarColour(el) {
   const colour = `hsl(${Math.floor(Math.random() * 16 * 16)}, 75%, 50%)`;
 
   el.style.background = colour;
 }
 
-function renderCar (car) {
+function renderCar(car) {
   const { x, y, angle, power, reverse, angularVelocity } = car;
 
-  car.el.style.transform = `translate(${x}px, ${y}px) rotate(${angle * 180 / Math.PI}deg)`;
+  car.el.style.transform = `translate(${x}px, ${y}px) rotate(${
+    (angle * 180) / Math.PI
+  }deg)`;
 
-  if ((power > 0.0025) || reverse) {
-    if (((maxReverse === reverse) || (maxPower === power)) && Math.abs(angularVelocity) < 0.002) {
+  if (power > 0.0025 || reverse) {
+    if (
+      (maxReverse === reverse || maxPower === power) &&
+      Math.abs(angularVelocity) < 0.002
+    ) {
       return;
     }
     ctx.fillRect(
-      x - Math.cos(angle + 3 * Math.PI / 2) * 3 + Math.cos(angle + 2 * Math.PI / 2) * 3,
-      y - Math.sin(angle + 3 * Math.PI / 2) * 3 + Math.sin(angle + 2 * Math.PI / 2) * 3,
+      x -
+        Math.cos(angle + (3 * Math.PI) / 2) * 3 +
+        Math.cos(angle + (2 * Math.PI) / 2) * 3,
+      y -
+        Math.sin(angle + (3 * Math.PI) / 2) * 3 +
+        Math.sin(angle + (2 * Math.PI) / 2) * 3,
       1,
       1
     );
     ctx.fillRect(
-      x - Math.cos(angle + 3 * Math.PI / 2) * 3 + Math.cos(angle + 4 * Math.PI / 2) * 3,
-      y - Math.sin(angle + 3 * Math.PI / 2) * 3 + Math.sin(angle + 4 * Math.PI / 2) * 3,
+      x -
+        Math.cos(angle + (3 * Math.PI) / 2) * 3 +
+        Math.cos(angle + (4 * Math.PI) / 2) * 3,
+      y -
+        Math.sin(angle + (3 * Math.PI) / 2) * 3 +
+        Math.sin(angle + (4 * Math.PI) / 2) * 3,
       1,
       1
     );
   }
 }
 
-function render (ms) {
+function render(ms) {
   requestAnimationFrame(render);
 
   if (needResize || resizing) {
@@ -303,7 +269,7 @@ function render (ms) {
         canvas.width = windowWidth;
         canvas.height = windowHeight;
 
-        ctx.fillStyle = 'rgba(63, 63, 63, 0.25)';
+        ctx.fillStyle = "rgba(63, 63, 63, 0.25)";
 
         ctx.drawImage(prevImage, 0, 0);
       };
@@ -315,7 +281,7 @@ function render (ms) {
 
 requestAnimationFrame(render);
 
-function resize () {
+function resize() {
   windowWidth = window.innerWidth;
   windowHeight = window.innerHeight;
 
@@ -324,118 +290,10 @@ function resize () {
 
 resize();
 
-window.addEventListener('resize', resize);
-
-const socket = io('https://car.pakastin.fi');
-
-socket.on('connect', () => {
-  sendParams(localCar);
-});
-
-socket.on('join', () => {
-  sendParams(localCar);
-});
-
-socket.on('params', ({ id, params }) => {
-  let car = carsById[id];
-
-  if (!car) {
-    const el = document.createElement('div');
-    el.classList.add('car');
-    scene.insertBefore(el, localCar.el);
-    car = {
-      el
-    };
-    carsById[id] = car;
-    cars.push(car);
-    el.addEventListener('click', () => randomizeCarColour(el));
-  }
-
-  if (params.ghost) {
-    car.el.classList.add('ghost');
-  }
-
-  for (const key in params) {
-    if (key !== 'el') {
-      car[key] = params[key];
-    }
-  }
-});
-
-socket.on('leave', (id) => {
-  const car = carsById[id];
-
-  if (!car) {
-    return console.error('Car not found');
-  }
-
-  for (let i = 0; i < cars.length; i++) {
-    if (cars[i] === car) {
-      cars.splice(i, 1);
-      break;
-    }
-  }
-
-  if (car.el.parentNode) {
-    car.el.parentNode.removeChild(car.el);
-  }
-  delete carsById[id];
-});
-
-function sendParams (car) {
-  const {
-    x,
-    y,
-    xVelocity,
-    yVelocity,
-    power,
-    reverse,
-    angle,
-    angularVelocity,
-    isThrottling,
-    isReversing,
-    isTurningLeft,
-    isTurningRight
-  } = car;
-
-  socket.emit('params', {
-    x,
-    y,
-    xVelocity,
-    yVelocity,
-    power,
-    reverse,
-    angle,
-    angularVelocity,
-    isThrottling,
-    isReversing,
-    isTurningLeft,
-    isTurningRight
-  });
-}
-
-const disconnect = document.getElementsByTagName('button')[0];
-
-disconnect.onclick = () => {
-  socket.disconnect();
-
-  while (cars.length > 1) {
-    const car = cars.pop();
-
-    car.el.parentNode.removeChild(car.el);
-  }
-
-  disconnect.parentNode.removeChild(disconnect);
-};
-
-const clearScreen = document.getElementsByTagName('button')[1];
-
-clearScreen.onclick = () => {
-  ctx.clearRect(0, 0, windowWidth, windowHeight);
-};
+window.addEventListener("resize", resize);
 
 setInterval(() => {
-  ctx.fillStyle = 'rgba(255, 255, 255, .05)';
+  ctx.fillStyle = "rgba(255, 255, 255, .05)";
   ctx.fillRect(0, 0, windowWidth, windowHeight);
-  ctx.fillStyle = 'rgba(63, 63, 63, 0.25)';
+  ctx.fillStyle = "rgba(63, 63, 63, 0.25)";
 }, 30000);
